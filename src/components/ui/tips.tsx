175fcaa97d8a7b933106bac18f220972a3e86204
akizon77 <hytf233g@gmail.com>
2025-09-20 19:37:56 +0800

    feat: 优化Tips组件

--- src/components/ui/tips.tsx
+++ src/components/ui/tips.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Info } from "lucide-react";
-import { Popover, Dialog } from "@radix-ui/themes";
+import { Popover, Dialog, Box } from "@radix-ui/themes";
 import { useIsMobile } from "@/hooks/use-mobile";
 
 interface TipsProps {
@@ -70,20 +70,20 @@ const Tips: React.FC<TipsProps & React.HTMLAttributes<HTMLDivElement>> = ({
             sideOffset={5}
             onMouseEnter={!isMobile ? () => setIsOpen(true) : undefined}
             onMouseLeave={!isMobile ? () => setIsOpen(false) : undefined}
-            style={{
-              padding: "0.5rem",
-              border: "none",
-              boxShadow:
-                "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
-              borderRadius: "var(--radius-3)",
-              zIndex: 5,
-              minWidth: isMobile ? "12rem" : "16rem",
-              maxWidth: isMobile ? "80vw" : "16rem",
-              backgroundColor: "var(--accent-3)",
-              color: "var(--gray-12)",
-            }}
+            // style={{
+            //   padding: "0.5rem",
+            //   border: "none",
+            //   boxShadow:
+            //     "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
+            //   borderRadius: "var(--radius-3)",
+            //   zIndex: 5,
+            //   minWidth: isMobile ? "12rem" : "16rem",
+            //   maxWidth: isMobile ? "80vw" : "16rem",
+            //   backgroundColor: "var(--accent-3)",
+            //   color: "var(--gray-12)",
+            // }}
           >
-            <div className="relative text-sm">{children}</div>
+            <Box className="relative text-sm min-w-48">{children}</Box>
           </Popover.Content>
         </Popover.Root>
       )}
