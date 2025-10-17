c3feca10af63aa51dc7a005f2576c1eb56ccc172
Akizon77 <i@akz.moe>
2025-10-17 23:32:28 +0800

    feat: 添加关于页面及开源软件许可信息

--- src/pages/admin/about.tsx
+++ src/pages/admin/about.tsx
@@ -3,9 +3,14 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import "github-markdown-css/github-markdown.css";
 import Loading from "@/components/loading";
+import { useTranslation } from "react-i18next";
+import { SquareArrowOutUpRight } from "lucide-react";
+import { SegmentedControl } from "@radix-ui/themes";
 
 export default function AboutPage() {
   const [markdown, setMarkdown] = useState("");
+  const { t } = useTranslation();
+  const [view, setView] = useState("readme");
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/komari-monitor/komari/refs/heads/main/README.md"
@@ -13,12 +18,151 @@ export default function AboutPage() {
       .then((res) => res.text())
       .then(setMarkdown);
   }, []);
+
+  const open_source_licenses = {
+    "Apache-2.0 License": [
+      "github.com/coreos/go-oidc/v3",
+      "github.com/pquerna/otp",
+      "github.com/spf13/cobra",
+      "google.golang.org/grpc",
+      "typescript",
+      "github.com/prometheus-community/pro-bing",
+    ],
+    "BSD-2-Clause License": ["github.com/gorilla/websocket", "dotenv"],
+    "BSD-3-Clause License": [
+      "github.com/google/uuid",
+      "google.golang.org/protobuf",
+      "golang.org/x/net",
+      "golang.org/x/crypto",
+      "golang.org/x/sys",
+      "github.com/shirou/gopsutil/v4",
+    ],
+    "MIT License": [
+      "github.com/gin-gonic/gin",
+      "github.com/patrickmn/go-cache",
+      "github.com/stretchr/testify",
+      "gorm.io/driver/mysql",
+      "gorm.io/driver/sqlite",
+      "gorm.io/gorm",
+      "@dnd-kit/core",
+      "@dnd-kit/modifiers",
+      "@dnd-kit/sortable",
+      "@radix-ui/react-checkbox",
+      "@radix-ui/react-dialog",
+      "@radix-ui/react-dropdown-menu",
+      "@radix-ui/react-icons",
+      "@radix-ui/react-label",
+      "@radix-ui/react-slot",
+      "@radix-ui/themes",
+      "@tanstack/react-table",
+      "@types/uuid",
+      "class-variance-authority",
+      "clsx",
+      "github-markdown-css",
+      "http-proxy-middleware",
+      "i18next",
+      "i18next-browser-languagedetector",
+      "motion",
+      "next-themes",
+      "react",
+      "react-dom",
+      "react-i18next",
+      "react-markdown",
+      "react-toastify",
+      "recharts",
+      "remark-gfm",
+      "sonner",
+      "tailwind-merge",
+      "tailwindcss",
+      "@tailwindcss/vite",
+      "twemoji",
+      "uuid",
+      "vaul",
+      "xterm",
+      "xterm-addon-fit",
+      "xterm-addon-search",
+      "xterm-addon-web-links",
+      "@eslint/js",
+      "@types/lodash",
+      "@types/react",
+      "@types/react-dom",
+      "@vitejs/plugin-react",
+      "eslint",
+      "eslint-plugin-react-hooks",
+      "eslint-plugin-react-refresh",
+      "globals",
+      "react-router-dom",
+      "rollup-plugin-visualizer",
+      "tw-animate-css",
+      "typescript-eslint",
+      "vite",
+      "vite-plugin-pages",
+      "vite-plugin-pwa",
+      "github.com/UserExistsError/conpty",
+      "github.com/blang/semver",
+      "github.com/creack/pty",
+      "github.com/go-ole/go-ole",
+      "github.com/klauspost/cpuid/v2",
+      "github.com/rhysd/go-github-selfupdate",
+      "gopkg.in/toast.v1",
+    ],
+    "ISC License": ["github.com/oschwald/maxminddb-golang", "lucide-react"],
+    "CC-BY-4.0 License": ["twemoji"],
+  };
+
+  const sortedLicenses = Object.entries(open_source_licenses).sort(([a], [b]) =>
+    a.localeCompare(b)
+  );
+
   return (
-    <div className="markdown-body p-4">
-      {markdown ? (
-        <ReactMarkdown remarkPlugins={[remarkGfm]} children={markdown} />
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold text-foreground">{t("about.title")}</h1>
+      <SegmentedControl.Root defaultValue={view} onValueChange={setView}>
+        <SegmentedControl.Item value="readme">Readme</SegmentedControl.Item>
+        <SegmentedControl.Item value="open_source">
+          {t("about.open_source_title")}
+        </SegmentedControl.Item>
+      </SegmentedControl.Root>
+      {view === "readme" ? (
+        <>
+          <div className="markdown-body border border-muted/20 rounded-md">
+            {markdown ? (
+              <ReactMarkdown remarkPlugins={[remarkGfm]} children={markdown} />
+            ) : (
+              <Loading />
+            )}
+          </div>
+          <a
+            href="https://github.com/komari-monitor/komari/blob/main/README.md"
+            target="_blank"
+            rel="noreferrer"
+            className="flex flex-row gap-2 text-sm items-center"
+          >
+            {t("about.readme_open_in_new_tab")}
+            <SquareArrowOutUpRight size="16"></SquareArrowOutUpRight>
+          </a>
+        </>
       ) : (
-        <Loading />
+        <>
+          <h2 className="text-xl font-semibold text-foreground">
+            {t("about.open_source")}
+          </h2>
+          <div className="copyright text-sm text-gray-500 dark:text-gray-400">
+            {sortedLicenses.map(([license, libs]) => (
+              <div key={license} className="mb-2">
+                <h3 className="font-black text-lg text-foreground">
+                  {license}
+                </h3>
+                <ul className="list-disc list-inside">
+                  {libs.sort().map((lib) => (
+                    <li key={lib}>{lib}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+            {t("about.ai")}
+          </div>
+        </>
       )}
     </div>
   );
