fadf640bdcb8a3d71e6538e40a3b9895a0ca0db8
Z <github@roflzombie.com>
2025-12-10 13:36:35 +0100

    Change wget command to use sudo for Linux
    Fixed mistake

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -653,7 +653,7 @@ function GenerateCommandButton({ node, settings }: { node: NodeDetail, settings:
     let finalCommand = "";
     switch (selectedPlatform) {
       case "linux":
-        finalCommand = `wget -qO- ${scriptUrl}) | bash -s -- ` + args.join(" ");
+        finalCommand = `wget -qO- ${scriptUrl} | sudo bash -s -- ` + args.join(" ");
         break;
       case "windows":
         finalCommand =
